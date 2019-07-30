fcb70f480b0bfb1065a5093c134546675491f6da
Bruno Martins <bgcngm@gmail.com>
2019-07-30 15:03:39 +0100

    sdm845-common: Compile thermal and VR HALs from source
    Change-Id: I4d15cbd56896a70c3ea6459f82d0e83d81cb5ca3

--- sdm845.mk
+++ sdm845.mk
@@ -275,7 +275,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm845
 
 # USB
 PRODUCT_PACKAGES += \
@@ -293,7 +294,8 @@ PRODUCT_PACKAGES += \
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service
+    android.hardware.vr@1.0-service \
+    vr.sdm845
 
 # WiFi
 PRODUCT_PACKAGES += \
